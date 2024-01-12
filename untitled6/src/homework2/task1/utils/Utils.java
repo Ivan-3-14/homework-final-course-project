90cd887441051b8362f211ffe41177a3b89d38b4
Ivan_3_14_ <9129946@mail.ru>
2024-01-12 16:53:01 +0300

    1fix

---
+++ untitled6/src/homework2/task1/utils/Utils.java
@@ -0,0 +1,10 @@
+package homework2.task1.utils;
+
+import homework2.task1.robot.RobotPart;
+
+public class Utils {
+    public static final int COUNT_OF_ROBOT_PART = RobotPart.values().length;
+    public static final int MAX_NUMBER_OF_THROWS_ROBOT_PART = 4;
+    public static final int COUNT_OF_NIGHT = 100;
+    public static final int DELAY_FOR_THROW_DETAILS = 100;
+}
