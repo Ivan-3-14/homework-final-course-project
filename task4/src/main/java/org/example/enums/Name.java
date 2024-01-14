ef139d469810e70a2cf8d6076bc6d2ac06bfa928
Ivan_3_14_ <9129946@mail.ru>
2024-01-14 06:09:11 +0300

    45

---
+++ task4/src/main/java/org/example/enums/Name.java
@@ -0,0 +1,28 @@
+package main.java.org.example.enums;
+
+public enum Name {
+    ALEX,
+    JOHN,
+    MAX,
+    VIKI,
+    KEVIN,
+    ANN,
+    KATE,
+    BORIS,
+    HELGA,
+    HARRY,
+    ROMEO,
+    ROBERTO,
+    RONALD,
+    GEORGE,
+    RAUL,
+    LILY,
+    TED,
+    BARNY,
+    ROBIN,
+    MARSHALL,
+    MONIKA,
+    LUISA,
+    ROSS,
+    FRED
+}
---
+++ task4/src/main/java/org/example/utility/Constant.java
@@ -0,0 +1,9 @@
+package main.java.org.example.utility;
+
+public class Constant {
+    public static final int MIN_AGE_OF_PERSON = 15;
+    public static final int MAX_AGE_OF_PERSON = 30;
+    public static final int COUNT_OF_PERSONS = 100;
+    public static final int MAX_AGE_TO_FILTER = 21;
+    public static String PATH_TO_FILE = "src\\main\\java\\org\\example\\person\\FileWithPath";
+}
