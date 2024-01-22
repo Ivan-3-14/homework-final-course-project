2c7ba6ca3c8674cbce8107df0f75da55c7ad9145
Ivan Pavliushchyk <131768267+Ivan-3-14@users.noreply.github.com>
2024-01-22 17:39:08 +0300

    Add files via upload

--- src/main/java/org/example/ConstantContainer.java
+++ src/main/java/org/example/ConstantContainer.java
@@ -7,4 +7,9 @@ public class ConstantContainer {
     public static final String INSERT_PERSON_PREPARE_STATEMENT =
             "insert into people.person(age, salary, passport, address,dateOfBirthday, dateTimeCreate, timeToLunch, letter)" +
                     "VALUES (?, ?, ?, ?, ?, ?, ?, ?);";
+
+    public static final String NAME_OF_TABLE_FOR_PRINT_META = "TABLE_NAME";
+    public static final String NAME_OF_COLUMNS_FOR_PRINT_META = "COLUMN_NAME";
+    public static final String TYPE_OF_COLUMNS_FOR_PRINT_META = "TYPE_NAME";
+    public static final String SIZE_OF_COLUMNS_FOR_PRINT_META = "COLUMN_SIZE";
 }
--- src/main/java/org/example/PrinterDatabaseInfo.java
+++ src/main/java/org/example/PrinterDatabaseInfo.java
@@ -4,16 +4,19 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.example.ConstantContainer.*;
+
 public class PrinterDatabaseInfo {
 
     public static void printTableMeta(Connection connection, String databaseName) throws SQLException {
         DatabaseMetaData metaData = connection.getMetaData();
         ResultSet rs = metaData.getTables(databaseName, null, null, null);
-        List tables = new ArrayList();
+        List<String> tables = new ArrayList();
         while (rs.next()) {
-            tables.add(rs.getString("TABLE_NAME"));
+            tables.add(rs.getString(NAME_OF_TABLE_FOR_PRINT_META));
         }
         getColumnsMetadata(tables, metaData);
+        rs.close();
     }
 
     private static void getColumnsMetadata(List<String> tables, DatabaseMetaData metaData) throws SQLException {
@@ -23,9 +26,9 @@ public class PrinterDatabaseInfo {
             System.out.println("table name: " + actualTable.toUpperCase());
             int count = 1;
             while (rs.next()) {
-                System.out.println(count + "), name: " + rs.getString("COLUMN_NAME") + ", type: "
-                        + rs.getString("TYPE_NAME") + ", size: "
-                        + rs.getString("COLUMN_SIZE")
+                System.out.println(count + "), name: " + rs.getString(NAME_OF_COLUMNS_FOR_PRINT_META) + ", type: "
+                        + rs.getString(TYPE_OF_COLUMNS_FOR_PRINT_META) + ", size: "
+                        + rs.getString(SIZE_OF_COLUMNS_FOR_PRINT_META)
                 );
                 count++;
             }
