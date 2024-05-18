bad0ede4a0d1488844d1b703a8f0fefb7ceb664d
Ivan Pavliushchyk <131768267+Ivan-3-14@users.noreply.github.com>
2024-05-18 20:55:33 +0300

    Delete concreteBelarus directory

--- concreteBelarus/src/main/java/application/utils/functionalinterface/MyInterfaceToDAO.java
+++
@@ -1,8 +0,0 @@
-package application.utils.functionalinterface;
-
-import javax.servlet.ServletException;
-import java.io.IOException;
-
-public interface MyInterfaceToDAO<T> {
-    T betweenBeginAndCommitted() throws ServletException, IOException;
-}
