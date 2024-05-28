8d98601f2bbf38c8871f8e754c4588c3c0bbac25
Ivan Pavliushchyk <131768267+Ivan-3-14@users.noreply.github.com>
2024-05-28 05:01:50 +0300

    Delete concreteBelarus directory

--- concreteBelarus/src/main/java/application/utils/enums/cartype/CarType.java
+++
@@ -1,8 +0,0 @@
-package application.utils.enums.cartype;
-
-public enum  CarType {
-
-    DUMP_TRUCK,
-    CONCRETE_MIXER_TRUCK
-
-}
--- concreteBelarus/src/main/java/application/utils/mappers/Mapper.java
+++
@@ -1,9 +0,0 @@
-package application.utils.mappers;
-
-public interface Mapper<T, R> {
-
-    R dtoToEntity(T t);
-
-    T entityToDTO(R r);
-
-}
